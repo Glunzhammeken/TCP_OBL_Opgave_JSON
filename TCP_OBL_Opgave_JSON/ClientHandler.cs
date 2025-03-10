0abe3b25c4751a045c41cf455cbc61fee3715056
Glunzhammeken <160604689+Glunzhammeken@users.noreply.github.com>
2025-03-10 19:34:50 +0100

    Made it so its the same json obj

--- TCP_OBL_Opgave_JSON/ClientHandler.cs
+++ TCP_OBL_Opgave_JSON/ClientHandler.cs
@@ -25,24 +25,25 @@ namespace TCP_OBL_Server_JSON
                     string? msg = reader.ReadLine();
                     
                     Json_Obj json_Obj = JsonSerializer.Deserialize<Json_Obj>(msg);
+
                     if (json_Obj.Method == "Random")
                     {
                         Random random = new Random();
-                        Json_Obj jsonResponse = new Json_Obj { Response = $"{random.Next(json_Obj.Num1, json_Obj.Num2)}" };
-                        writer.WriteLine($"{JsonSerializer.Serialize<Json_Obj>(jsonResponse)}");
+                        json_Obj.Response = $"{random.Next(json_Obj.Num1, json_Obj.Num2)}";
+                        writer.WriteLine($"{JsonSerializer.Serialize<Json_Obj>(json_Obj)}");
 
 
                     }
                     else if (json_Obj.Method == "Add")
                     {
-                        Json_Obj jsonResponse1 = new Json_Obj { Response = $"{(json_Obj.Num1 + json_Obj.Num2)}" };
-                        writer.WriteLine($"{JsonSerializer.Serialize<Json_Obj>(jsonResponse1)}");
+                        json_Obj.Response = $"{(json_Obj.Num1 + json_Obj.Num2)}";
+                        writer.WriteLine($"{JsonSerializer.Serialize<Json_Obj>(json_Obj)}");
                     }
                     else if (json_Obj.Method == "Subtract")
                     {
 
-                        Json_Obj jsonResponse2 = new Json_Obj { Response = $"{(json_Obj.Num1 - json_Obj.Num2)}" };
-                        writer.WriteLine($"{JsonSerializer.Serialize<Json_Obj>(jsonResponse2)}");
+                        json_Obj.Response  = $"{(json_Obj.Num1 - json_Obj.Num2)}" ;
+                        writer.WriteLine($"{JsonSerializer.Serialize<Json_Obj>(json_Obj)}");
                     }
                     else if (json_Obj.Method == "close")
                     {
